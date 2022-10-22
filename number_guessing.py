e84a19bc731a630cba7b043fe2bf1ef2946396eb
olivermol <111631561+olivermol@users.noreply.github.com>
2022-10-22 03:11:10 +0200

    Commited

--- number_guessing.py
+++ number_guessing.py
@@ -20,9 +20,9 @@ def start_game():
                 continue
         except ValueError:
             print("Message: Please, use ONLY number between 1 and 100")
-    #   3. Continuously prompt the player for a guess.
-    #       a. If the guess greater than the solution, display to the player "It's lower".
-    #       b. If the guess is less than the solution, display to the player "It's higher".
+#   3. Continuously prompt the player for a guess.
+#       a. If the guess greater than the solution, display to the player "It's lower".
+#       b. If the guess is less than the solution, display to the player "It's higher".
         else:
             attempts += 1
             if answer > guessed_number:
@@ -32,12 +32,15 @@ def start_game():
                 print("{} is too low, try a higher number".format(answer))
                 continue
             else:
+#  4. Once the guess is correct, stop looping, inform the user they "Got it" 
+# and show how many attempts it took them to get the correct number.
                 answer_list.append(attempts)
                 print("\n ========================== \n")
                 print("\n *** You are rock! You've figured out what was the guessed number! ***")
                 print("\n ========================== \n")
                 print("\n  ***  Your statics:  *** \n ========================== \n")
-                
+# Statitics about attempts
+# And show it median, mode and mean                
                 print("-==WINNER NUMBER: ==- \n")
                 print("Your WINNER NUMBER: {}".format(answer))
                 print(f"Median of your attempts number is: {median(answer_list)}")
@@ -46,7 +49,9 @@ def start_game():
                 print("\n ========================== \n")
             
             
-            
+# New game chooser
+# If say "Yes" then start the game (and show last score)
+# If say "No" then close the game            
             while True:
                 try:
                     new_game = input("\n *** Would you like to try again? ***\n(Please use 'y' = Yes or 'n' = No) : ")
